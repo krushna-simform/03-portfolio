350d030a9be736db92329fe819f634519a9886c2
krushna rathod <krushna.rathod@simformsolutions.com>
2025-05-16 16:50:53 +0530

    FEATURE: delete MaskShowcaseDetails file and update ShowcaseDetails file for DRY code

--- src/components/ui/ShowcaseDetails.tsx
+++ src/components/ui/ShowcaseDetails.tsx
@@ -1,21 +1,19 @@
+import { ShowcaseDetailsType as ShowcaseDetailsProps } from "@/types/types";
 import showcaseImage from "/images/showcase.png";
 
-export const ShowCaseDetails = () => {
+export const ShowcaseDetails = ({
+  nameOrRole,
+  summary,
+}: ShowcaseDetailsProps) => {
   return (
     <div className="md:w-[60%] px-4 mx-auto flex items-center h-full justify-between">
       <div className="w-[536px] h-[357px] flex flex-col justify-center">
         <p>Hey, I'm</p>
-        <p
-          className="text-[40px] mt-2 font-semibold"
-          aria-label="Krushna Rathod - Frontend Developer"
-        >
-          Krushna Rathod
+        <p className="text-[40px] mt-2 font-semibold" aria-label={nameOrRole}>
+          {nameOrRole}
         </p>
-        <p className="text-[18px] mt-2" aria-label="Developer Summary">
-          I'm a frontend developer who builds fast, responsive web apps with
-          React, focusing on performance, accessibility, and clean UI. I write
-          modular, maintainable code and enjoy solving real problems through
-          intuitive interfaces.
+        <p className="text-[18px] mt-2" aria-label="Summary">
+          {summary}
         </p>
       </div>
       <div className="hidden lg:block lg:w-[540px]">
