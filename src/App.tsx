252ac635c7e403567cd317524d5ff793342a7106
krushna rathod <krushna.rathod@simformsolutions.com>
2025-05-16 11:35:02 +0530

    FEATURE: add Main page into App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
+import Main from "@/pages/Main";
+
 function App() {
-  return <div className="text-red-700">Hello, World</div>;
+  return (
+    <>
+      <Main />
+    </>
+  );
 }
 
 export default App;
