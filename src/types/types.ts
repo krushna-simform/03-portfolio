dcfc30e1c4c9354b1fef0e5f73fec98b7cdb4291
krushna rathod <krushna.rathod@simformsolutions.com>
2025-05-16 16:47:56 +0530

    FEATURE: move hooks and types folder outside of the utility folder

---
+++ src/types/types.ts
@@ -0,0 +1,11 @@
+interface NavbarLinkType {
+  label: string;
+  href: string;
+}
+
+interface ShowcaseDetailsType {
+  nameOrRole: string;
+  summary: string;
+}
+
+export type { NavbarLinkType, ShowcaseDetailsType };
