ff9cdd9cbca7efb0c7b77ec297ac5f4c9159731d
krushna rathod <krushna.rathod@simformsolutions.com>
2025-05-16 14:03:31 +0530

    FEATURE: create ShowCase component

---
+++ src/components/ShowCase.tsx
@@ -0,0 +1,44 @@
+import { useState } from "react";
+import { motion } from "framer-motion";
+import useMousePosition from "@/utils/hooks/useMousePosition";
+import { ShowCaseDetails } from "@/components/ui/ShowCaseDetails";
+import { MaskShowCaseDetails } from "@/components/ui/MaskShowCaseDetails";
+
+const ShowCase = () => {
+  const [isHovered, setIsHovered] = useState(false);
+  const { x, y } = useMousePosition();
+  const size = isHovered ? 320 : 0;
+
+  return (
+    <section className="relative h-[678.844px] bg-[#0A0A0A] text-white flex items-center mt-[55px] overflow-hidden">
+      <motion.div
+        className={`absolute top-0 left-0 w-full h-full z-10`}
+        animate={{
+          opacity: isHovered ? 1 : 0,
+          WebkitMaskPosition: `${x - size / 2}px ${y - size / 2}px`,
+          WebkitMaskSize: `${size}px`,
+        }}
+        transition={{ type: "tween", ease: "backOut", duration: 0.5 }}
+        style={{
+          WebkitMaskImage: "url('/images/mask.svg')",
+          maskImage: "url('/images/mask.svg')",
+          WebkitMaskRepeat: "no-repeat",
+          maskRepeat: "no-repeat",
+          background: "#0000FE",
+          pointerEvents: "none",
+        }}
+      >
+        <MaskShowCaseDetails />
+      </motion.div>
+      <div
+        className="w-full h-full"
+        onMouseEnter={() => setIsHovered(true)}
+        onMouseLeave={() => setIsHovered(false)}
+      >
+        <ShowCaseDetails />
+      </div>
+    </section>
+  );
+};
+
+export default ShowCase;
