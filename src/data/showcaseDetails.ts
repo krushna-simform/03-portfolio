14adfdbd62e31dfff02907ed5b80695e487129b3
krushna rathod <krushna.rathod@simformsolutions.com>
2025-05-19 14:13:33 +0530

    FEATURE: add SHOCASE_DETAILS_NAME_SUMMARY and SHOWCASE_DETAILS_ROLE_SUMMARY objects in one array

--- src/data/showcaseDetails.ts
+++ src/data/showcaseDetails.ts
@@ -1,15 +1,16 @@
 import type { ShowcaseDetails } from "@/types/types";
 
-const SHOWCASE_DETAILS_NAME_SUMMARY: ShowcaseDetails = {
-  title: "Krushna Rathod",
-  summary:
-    "I'm a frontend developer who builds fast, responsive web apps with React, focusing on performance, accessibility, and clean UI. I write modular, maintainable code and enjoy solving real problems through intuitive interfaces.",
-};
+const SHOWCASE_DETAILS_TITLE_SUMMARY: Array<ShowcaseDetails> = [
+  {
+    title: "Krushna Rathod",
+    summary:
+      "I'm a frontend developer who builds fast, responsive web apps with React, focusing on performance, accessibility, and clean UI. I write modular, maintainable code and enjoy solving real problems through intuitive interfaces.",
+  },
+  {
+    title: "Frontend Developer",
+    summary:
+      "I’m always exploring new tools in the frontend ecosystem to push the boundaries of what’s possible. From optimizing performance to refining component architecture, I thrive on turning complex challenges into seamless user experiences.",
+  },
+];
 
-const SHOWCASE_DETAILS_ROLE_SUMMARY: ShowcaseDetails = {
-  title: "Frontend Developer",
-  summary:
-    "I’m always exploring new tools in the frontend ecosystem to push the boundaries of what’s possible. From optimizing performance to refining component architecture, I thrive on turning complex challenges into seamless user experiences.",
-};
-
-export { SHOWCASE_DETAILS_NAME_SUMMARY, SHOWCASE_DETAILS_ROLE_SUMMARY };
+export { SHOWCASE_DETAILS_TITLE_SUMMARY };
