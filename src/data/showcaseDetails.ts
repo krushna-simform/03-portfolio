266d46c1082f2f765b10899af985069a29f46a83
krushna rathod <krushna.rathod@simformsolutions.com>
2025-05-19 12:17:03 +0530

    FEATURE: update shocaseDetails based on updated interface

--- src/data/showcaseDetails.ts
+++ src/data/showcaseDetails.ts
@@ -1,13 +1,13 @@
-import type { ShowcaseDetailsType } from "@/types/types";
+import type { ShowcaseDetails } from "@/types/types";
 
-const SHOWCASE_DETAILS_NAME_SUMMARY: ShowcaseDetailsType = {
-  nameOrRole: "Krushna Rathod",
+const SHOWCASE_DETAILS_NAME_SUMMARY: ShowcaseDetails = {
+  title: "Krushna Rathod",
   summary:
     "I'm a frontend developer who builds fast, responsive web apps with React, focusing on performance, accessibility, and clean UI. I write modular, maintainable code and enjoy solving real problems through intuitive interfaces.",
 };
 
-const SHOWCASE_DETAILS_ROLE_SUMMARY: ShowcaseDetailsType = {
-  nameOrRole: "Frontend Developer",
+const SHOWCASE_DETAILS_ROLE_SUMMARY: ShowcaseDetails = {
+  title: "Frontend Developer",
   summary:
     "I’m always exploring new tools in the frontend ecosystem to push the boundaries of what’s possible. From optimizing performance to refining component architecture, I thrive on turning complex challenges into seamless user experiences.",
 };
