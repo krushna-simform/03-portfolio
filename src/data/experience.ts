e30c1a3d1083f8f88e44dd95a758bd20e1cda6f8
krushna rathod <krushna.rathod@simformsolutions.com>
2025-05-20 15:05:20 +0530

    FEATURE: add experience for data

---
+++ src/data/experience.ts
@@ -0,0 +1,24 @@
+import { Experience } from "@/types/types";
+
+const EXPERIENCE: Array<Experience> = [
+  {
+    company: "Simform Software LLP.",
+    role: "ReactJS Trainee",
+    description:
+      "This internship focuses on learning React.js, enhancing problem-solving and communication skills, gaining team leadership experience, and building a reliable, accessible frontend with proper code reviews.",
+    city: "Ahmedabad",
+    from: "January 2025",
+    to: "present",
+  },
+  {
+    company: "TatvaSoft Pvt. Ltd.",
+    role: "Web Dev Trainee",
+    description:
+      "A project focused on providing Virtual Community Support through the integration of frontend with Angular Framework and backend with .Net along with PostgreSQL to provide an easy way to access data for user.",
+    city: "Ahmedabad",
+    from: "May 2024",
+    to: "June 2024",
+  },
+];
+
+export { EXPERIENCE };
