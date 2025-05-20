945433303c96f8e64a8fc1bd9edfade8a0fe22c4
krushna rathod <krushna.rathod@simformsolutions.com>
2025-05-20 12:35:36 +0530

    FEATURE: create skills.ts files for skill section

---
+++ src/data/skills.ts
@@ -0,0 +1,96 @@
+import { Skills } from "@/types/types";
+
+const SKILLS: Array<Skills> = [
+  {
+    name: "html",
+    path: "/icon/html.svg",
+    url: "https://developer.mozilla.org/en-US/docs/Web/HTML",
+  },
+  {
+    name: "css",
+    path: "/icon/css.svg",
+    url: "https://developer.mozilla.org/en-US/docs/Web/CSS",
+  },
+  {
+    name: "javascript",
+    path: "/icon/javascript.svg",
+    url: "https://developer.mozilla.org/en-US/docs/Web/JavaScript",
+  },
+  {
+    name: "typescript",
+    path: "/icon/typescript.svg",
+    url: "https://www.typescriptlang.org/",
+  },
+  {
+    name: "react",
+    path: "/icon/react.svg",
+    url: "https://react.dev/",
+  },
+  {
+    name: "tailwindcss",
+    path: "/icon/tailwindcss.svg",
+    url: "https://tailwindcss.com/",
+  },
+  {
+    name: "vite",
+    path: "/icon/vite.svg",
+    url: "https://vite.dev/",
+  },
+  {
+    name: "storybook",
+    path: "/icon/storybook.svg",
+    url: "https://storybook.js.org/",
+  },
+  {
+    name: "figma",
+    path: "/icon/figma.svg",
+    url: "https://www.figma.com/",
+  },
+  {
+    name: "vscode",
+    path: "/icon/vscode.svg",
+    url: "https://code.visualstudio.com/",
+  },
+  {
+    name: "nodejs",
+    path: "/icon/nodejs.svg",
+    url: "https://nodejs.org/",
+  },
+  {
+    name: "appwrite",
+    path: "/icon/appwrite.svg",
+    url: "https://appwrite.io/",
+  },
+  {
+    name: "mongodb",
+    path: "/icon/mongodb.svg",
+    url: "https://www.mongodb.com/",
+  },
+  {
+    name: "postgres",
+    path: "/icon/postgres.svg",
+    url: "https://www.postgresql.org/",
+  },
+  {
+    name: "git",
+    path: "/icon/git.svg",
+    url: "https://git-scm.com/",
+  },
+  {
+    name: "github",
+    path: "/icon/github.svg",
+    url: "https://github.com/",
+  },
+  {
+    name: "c",
+    path: "/icon/c.svg",
+    url: "https://en.wikipedia.org/wiki/C_(programming_language)",
+  },
+  {
+    name: "linux",
+    path: "/icon/linux.svg",
+    url: "https://www.linux.org/",
+  },
+];
+
+export { SKILLS };
