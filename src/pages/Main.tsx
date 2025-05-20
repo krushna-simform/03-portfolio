5af91754e15e7b26aa02a892ccb08226959b10cc
krushna rathod <krushna.rathod@simformsolutions.com>
2025-05-20 12:38:14 +0530

    FEATURE: import Skill component in Main page

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,11 +1,13 @@
 import { Header } from "@/components/Header";
 import { Showcase } from "@/components/Showcase";
+import { Skills } from "@/components/Skills";
 
 const Main = () => {
   return (
     <>
       <Header />
       <Showcase />
+      <Skills />
     </>
   );
 };
