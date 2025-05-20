6111a75f8bb5b0a2c91def7c65e1b10e6ea6b2bd
krushna rathod <krushna.rathod@simformsolutions.com>
2025-05-20 12:36:54 +0530

    FEATURE: create tagline for porfolio

---
+++ src/components/ui/Tagline.tsx
@@ -0,0 +1,10 @@
+export const Tagline = () => {
+  return (
+    <div className="flex justify-center text-center">
+      <p className="text-2xl md:text-[34px] space-y-[24px] w-[600px]">
+        Not everything should be visible on the screen — some magic runs behind
+        the scenes in every well-built app.
+      </p>
+    </div>
+  );
+};
