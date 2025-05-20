4bf84de729a7db476f15e9e90f90f4dbebda8dd8
krushna rathod <krushna.rathod@simformsolutions.com>
2025-05-20 14:15:23 +0530

    FEATURE: add data for education section

---
+++ src/data/education.ts
@@ -0,0 +1,20 @@
+import type { Education } from "@/types/types";
+
+const EDUCATION: Array<Education> = [
+  {
+    course: "Bachelors in Information Technology",
+    institute: "A. D. Patel Institute of Technology",
+    city: "Anand",
+    from: 2021,
+    to: 2025,
+  },
+  {
+    course: "Higher Secondary Education",
+    institute: "Shree M. B. Vamdom School",
+    city: "Bardoli - Surat",
+    from: 2019,
+    to: 2021,
+  },
+];
+
+export { EDUCATION };
