d90e8c668bff05ad21e3391dbc445e1a496d1249
krushna rathod <krushna.rathod@simformsolutions.com>
2025-05-20 14:17:10 +0530

    FEATURE: create education component

---
+++ src/components/ui/Education.tsx
@@ -0,0 +1,21 @@
+import type { Education as EducationProps } from "@/types/types";
+
+export const Education = ({
+  course,
+  institute,
+  city,
+  from,
+  to,
+}: EducationProps) => {
+  return (
+    <div className="flex flex-col justify-center space-y-2">
+      <div className="space-y-2">
+        <p className="text-2xl">{course}</p>
+        <div>
+          <p className="text-lg">{`${institute}, ${city}`}</p>
+        </div>
+      </div>
+      <p className="text-lg">{`${from} - ${to}`}</p>
+    </div>
+  );
+};
