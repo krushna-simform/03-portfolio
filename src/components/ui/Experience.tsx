be9486f4953fbd82101fa9867fb7eda28e924e29
krushna rathod <krushna.rathod@simformsolutions.com>
2025-05-20 15:06:18 +0530

    FEATURE: create experience component

---
+++ src/components/ui/Experience.tsx
@@ -0,0 +1,23 @@
+import { Experience as ExperienceProps } from "@/types/types";
+
+export const Experience = ({
+  company,
+  city,
+  description,
+  role,
+  from,
+  to,
+}: ExperienceProps) => {
+  return (
+    <div className="flex flex-col justify-center space-y-2">
+      <div className="space-y-2">
+        <p className="text-2xl">{role}</p>
+        <div className="space-y-2">
+          <p className="text-lg">{`${company}, ${city}`}</p>
+          <p className="text-[16px] text-gray-600">{description}</p>
+        </div>
+      </div>
+      <p className="text-lg">{`${from} - ${to}`}</p>
+    </div>
+  );
+};
