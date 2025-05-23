666fface030cc3bc53366f9be008ccf00985b528
krushna rathod <krushna.rathod@simformsolutions.com>
2025-05-23 17:00:46 +0530

    FEATURE: create footer comonent contect links dry

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,5 @@
 import { LineAttractor } from "@/components/ui/LineAttractor";
-import { FaLinkedinIn } from "react-icons/fa";
-import { FaGithub } from "react-icons/fa";
-import { IoMdMail } from "react-icons/io";
+import { CONTACT_LINKS } from "@/data/contactLinks";
 
 export const Footer = () => {
   return (
@@ -11,33 +9,17 @@ export const Footer = () => {
         <div className="mt-10 text-2xl">
           <p className="text-white text-2xl">Contact Me</p>
           <div className="mt-6 flex flex-wrap gap-3">
-            <a
-              href="https://www.linkedin.com/in/krushna-rathod"
-              target="_blank"
-              aria-label="LinkedIn link (opens in a new tab)"
-              className="inline-flex items-center gap-2 text-white bg-[#2727FE] py-2 px-8 rounded-full"
-            >
-              <FaLinkedinIn />
-              LinkedIn
-            </a>
-            <a
-              href="https://www.github.com/krushna-simform"
-              target="_blank"
-              aria-label="GitHub link (opens in a new tab)"
-              className="inline-flex items-center gap-2 text-white bg-[#2727FE] py-2 px-8 rounded-full"
-            >
-              <FaGithub />
-              GitHub
-            </a>
-            <a
-              href="mailto:krushna.rathod@simformsolutions.com"
-              target="_blank"
-              aria-label="Mail link (opens in a new tab)"
-              className="inline-flex items-center gap-2 text-white bg-[#2727FE] py-2 px-8 rounded-full"
-            >
-              <IoMdMail />
-              Mail
-            </a>
+            {CONTACT_LINKS.map((contacts) => (
+              <a
+                href={contacts.link}
+                target="_blank"
+                aria-label={`${contacts.ariaLabel} link (opens in a new tab)`}
+                className="inline-flex items-center gap-2 text-white bg-[#2727FE] py-2 px-8 rounded-full"
+                key={contacts.id}
+              >
+                {contacts.children}
+              </a>
+            ))}
           </div>
         </div>
       </div>
